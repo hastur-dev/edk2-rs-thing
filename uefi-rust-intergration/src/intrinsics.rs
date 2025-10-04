4b920428e910e7ed695ebb36bb9e32258e7b05fe
Ian Mizer <flaminwarnecro@gmail.com>
2025-10-04 06:48:42 -0400

    double definitions somewhere. comments added

--- uefi-rust-intergration/src/intrinsics.rs
+++ uefi-rust-intergration/src/intrinsics.rs
@@ -53,6 +53,21 @@ pub mod mem {
     // Note: memcmp is now provided by compiler_builtins
     // #[no_mangle]
     // pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
+    //     let mut i = 0;
+    //     while i < n {
+    //         let a = *s1.add(i);
+    //         let b = *s2.add(i);
+    //         if a != b {
+    //             return a as i32 - b as i32;
+    //         }
+    //         i += 1;
+    //     }
+    //     0
+    // }
+
+    /// bcmp implementation (same as memcmp for most purposes)
+    #[no_mangle]
+    pub unsafe extern "C" fn bcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
         let mut i = 0;
         while i < n {
             let a = *s1.add(i);
@@ -63,12 +78,6 @@ pub mod mem {
             i += 1;
         }
         0
-    }
-
-    /// bcmp implementation (same as memcmp for most purposes)
-    #[no_mangle]
-    pub unsafe extern "C" fn bcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
-        memcmp(s1, s2, n)
     }
 }
 
